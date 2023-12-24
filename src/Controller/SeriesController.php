f37c9a05debf6284ff7ee205bd4d167013b6f2e4
marcoantn020 <marcoantn020@gmail.com>
2023-12-24 10:09:21 -0300

    Controller

---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeriesController extends AbstractController
+{
+    #[Route('/series', name: 'app_series', methods: ['GET'])]
+    public function index(): Response
+    {
+        $seriesList = [
+            'Lost',
+            'Grey\'s Anatomy',
+            'Loki',
+            'Suits'
+        ];
+
+        return $this->render('series/index.html.twig', [
+            'controller_name' => 'SeriesController',
+            compact('seriesList')
+        ]);
+    }
+}
